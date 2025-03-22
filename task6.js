7dfe16fb12540de1498db1e8cea22d3633e34a41
unknown <anika01839@gmail.com>
2025-03-22 23:21:37 +0600

    task-6 aded

---
+++ task6.js
@@ -0,0 +1,9 @@
+/*
+// Write a Function to Convert Celsius to Fahrenheit
+
+function celsiusToFahrenheit(celsius) {
+  let result = (celsius * 9) / 5 + 32;
+  console.log("temperature : " + result + "F");
+}
+celsiusToFahrenheit(9);
+*/
