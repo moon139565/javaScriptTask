57ad08077ce4ebe045c5975599d76db773c6c348
unknown <anika01839@gmail.com>
2025-03-23 00:06:06 +0600

    some task complete

--- task6.js
+++ task6.js
@@ -6,4 +6,33 @@ function celsiusToFahrenheit(celsius) {
   console.log("temperature : " + result + "F");
 }
 celsiusToFahrenheit(9);
+
+// 2. Create a Function to Find the Maximum of Two Numbers
+
+function findMax(num1, num2) {
+  if (num1 > num2) {
+    console.log(`${num1} is larger then ${num2}`);
+  } else if (num1 === num2) {
+    console.log(`${num1} and ${num2} are equal`);
+  } else {
+    console.log(`${num2} is larger then ${num1}`);
+  }
+}
+
+findMax(1, -2);
+
+
+// 3. Function to Check if a String is a Palindrome
+
+function isPalindrome(str) {
+  let rev = str.split("").reverse().join("");
+
+  if (str == rev) {
+    console.log("is it Palindrome");
+  } else {
+    console.log("This is not Palindrome");
+  }
+}
+
+isPalindrome("mom");
 */
