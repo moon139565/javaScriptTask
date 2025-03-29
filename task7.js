2283a13de81f2d865757ef97aa1bc4f7976db3b0
unknown <anika01839@gmail.com>
2025-03-29 15:46:26 +0600

    rps code start

--- task7.js
+++ task7.js
@@ -1,7 +1,24 @@
 // Change the RPS Project
-function rpsProject() {}
+
+function rpsProject() {
+  let userInput = prompt("please inter rock, peper or seaser:");
+  let userChoice = userInput.toLocaleLowerCase();
+
+  let computerChoice = Math.floor(Math.random() * 3 + 1);
+  let computerInput = "";
+  if (computerChoice === 1) {
+    computerInput += "rock";
+  } else if (computerChoice === 2) {
+    computerInput += "peper";
+  } else if (computerChoice === 3) {
+    computerInput += "seaser";
+  }
+
+  console.log(computerInput);
+  console.log(computerChoice);
+}
+rpsProject();
+
 // 2. Build the Secret Number Guessing Game
 
-function new(params) {
-    
-}
+function neew(params) {}
